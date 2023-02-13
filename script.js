2cd7e1d8cfdd998c751802e3a651bbee922d2625
amirsudo <rezadehgani59@gmail.com>
2023-02-13 17:16:26 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+﻿let $ = document
+let locationKey = $.querySelector('#location')
+let title = $.querySelector('title')
+let keyElem = $.querySelector('#key')
+let locationElem = $.querySelector('#location')
+let whichElem = $.querySelector('#which')
+let codeElem = $.querySelector('#code')
+let keyCodeElem = $.querySelector('#keyCode')
+
+
+
+document.body.addEventListener('keydown', function (event) {
+	
+	event.preventDefault()
+	
+	starter.style.display = 'none'
+	heading.style.display = 'flex'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+
+	console.log(event)
+
+	let eventKey = event.key
+	let eventLocation = event.location
+	let eventWhich = event.which
+	let eventCode = event.code
+	let eventKeyCode = event.keyCode
+
+	keyElem.innerHTML = eventKey
+	locationElem.innerHTML = eventLocation
+	whichElem.innerHTML = eventWhich
+	codeElem.innerHTML = eventCode
+	keyCodeElem.innerHTML = eventKeyCode
+
+
+})
